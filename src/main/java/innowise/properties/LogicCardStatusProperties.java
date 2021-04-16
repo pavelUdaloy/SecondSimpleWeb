2d86507e8d022fb7670ab215b520ce20d6a8d2ff
pavelUdaloy <paveludaloi@gmail.com>
2021-04-16 03:37:46 +0300

    added logic status props yml

---
+++ src/main/java/innowise/properties/LogicCardStatusProperties.java
@@ -0,0 +1,19 @@
+package innowise.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import java.util.List;
+import java.util.Map;
+
+@Configuration
+@ConfigurationProperties(prefix = "logic-status")
+@PropertySource(value = "classpath:logicstatus.yml", factory = LogicCardStatusSourceFactory.class)
+@Getter
+@Setter
+public class LogicCardStatusProperties {
+    private Map<String, String> logicStatuses;
+}
