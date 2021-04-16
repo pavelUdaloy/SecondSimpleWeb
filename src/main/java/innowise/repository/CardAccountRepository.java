131ad9ff06f877c6effdda9329b610c2c46364da
pavelUdaloy <paveludaloi@gmail.com>
2021-04-16 16:01:38 +0300

    added columns to emp table

--- src/main/java/innowise/repository/CardAccountRepository.java
+++ src/main/java/innowise/repository/CardAccountRepository.java
@@ -1,6 +1,5 @@
 package innowise.repository;
 
-import innowise.entity.Card;
 import innowise.entity.CardAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
