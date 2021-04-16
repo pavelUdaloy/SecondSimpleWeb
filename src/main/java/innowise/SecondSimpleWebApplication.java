af960529e302314757d66745a003a33e5ce2835f
pavelUdaloy <paveludaloi@gmail.com>
2021-04-16 03:39:11 +0300

    fixed code with props

--- src/main/java/innowise/SecondSimpleWebApplication.java
+++ src/main/java/innowise/SecondSimpleWebApplication.java
@@ -1,27 +1,11 @@
 package innowise;
 
-import innowise.properties.LogicCardStatusProperties;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @SpringBootApplication
-public class SecondSimpleWebApplication implements CommandLineRunner {
+public class SecondSimpleWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(SecondSimpleWebApplication.class, args);
-    }
-
-    @Autowired
-    private LogicCardStatusProperties logicCardStatusProperties;
-
-    @Override
-    public void run(String... args) throws Exception {
-        for (Map.Entry<String, String> stringStringEntry : logicCardStatusProperties.getLogicStatuses().entrySet()) {
-            System.out.println(stringStringEntry);
-        }
     }
 }
--- src/main/java/innowise/properties/LogicCardStatusProperties.java
+++ src/main/java/innowise/properties/LogicCardStatusProperties.java
@@ -6,7 +6,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-import java.util.List;
 import java.util.Map;
 
 @Configuration
