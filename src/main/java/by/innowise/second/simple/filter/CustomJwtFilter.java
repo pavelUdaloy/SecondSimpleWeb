d4eda27befefa2fa608b1bab1a1643faed558c06
pavelUdaloy <paveludaloi@gmail.com>
2021-04-20 14:11:38 +0300

    added some url ignore

--- src/main/java/by/innowise/second/simple/filter/CustomJwtFilter.java
+++ src/main/java/by/innowise/second/simple/filter/CustomJwtFilter.java
@@ -3,40 +3,40 @@ package by.innowise.second.simple.filter;
 import by.innowise.second.simple.security.JwtUtil;
 import by.innowise.second.simple.service.UserService;
 import lombok.AllArgsConstructor;
-import lombok.SneakyThrows;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
-import javax.servlet.http.HttpFilter;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Component
 @AllArgsConstructor
-public class CustomJwtFilter extends HttpFilter {
-
+public class CustomJwtFilter extends OncePerRequestFilter {
     private final JwtUtil jwtTokenUtil;
     private final UserService userService;
 
     private final String AUTHORIZATION = "Authorization";
     private final String BEARER = "Bearer ";
 
-    @SneakyThrows
     @Override
-    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) {
-        if (request.getRequestURI().equals("/auth")) {
-            super.doFilter(request, response, chain);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String jwtToken = extractJwtFromRequest(request);
+        userService.setUser(jwtTokenUtil.getUsernameFromToken(jwtToken), "");
+        if (StringUtils.hasText(jwtToken) && jwtTokenUtil.validateToken(jwtToken)) {
+            super.doFilter(request, response, filterChain);
         } else {
-            String jwtToken = extractJwtFromRequest(request);
-            userService.setUser(jwtTokenUtil.getUsernameFromToken(jwtToken), "");
-            if (StringUtils.hasText(jwtToken) && jwtTokenUtil.validateToken(jwtToken)) {
-                super.doFilter(request, response, chain);
-            } else {
-                throw new BadCredentialsException("Wrong token!");
-            }
+            throw new BadCredentialsException("Wrong token!");
         }
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        return "/auth".equals(request.getRequestURI());
     }
 
     private String extractJwtFromRequest(HttpServletRequest request) {
